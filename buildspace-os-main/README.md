8ab33bec8d743c11d3e46baa12ca9625fe79234a
Arhaan Girdhar <17arhaan@gmail.com>
2024-06-28 00:00:55 +0530

    test 01

---
+++ buildspace-os-main/README.md
@@ -0,0 +1,33 @@
+# buildspace os
+
+## setup
+### you don't need to write any code or know how to code to set this up. It will take 2 mins max — promise
+
+**we even made a 2 min vid for you**
+
+[installing buildspcae os](https://github.com/buildspace/buildspace-os/assets/13204620/c19da4b4-1266-4772-ba4e-0fdd795df9f4)
+
+### 1. download
+click the green "Code" button and select "Download ZIP". make sure to extract the download in a safe spot!<br>
+![](https://i.imgur.com/TlzzORw.png)
+
+### 2. turn on Developer mode
+in your browser head to the "Extensions" section. this can be found in the settings portion of your browser! checkout the screenshot below:<br>
+![](https://i.imgur.com/igEIfnt.png)
+
+once you get there you'll see "Developer mode" in the top right. make sure to toggle that **on** and then you'll see new options (we're gonna be using "Load unpacked")<br>
+![](https://i.imgur.com/l8GLD4b.png)
+
+### 3. add the app to your browser
+you're at the home stretch! press the "Load unpacked" option and then go to the folder where you saved buildspace os. it should look something like this:<br>
+![](https://i.imgur.com/ztCAc8i.png)
+
+once you select it you should see this (nice you did it!)<br>
+![](https://i.imgur.com/VmpDMFq.png)
+
+### 4. open a new tab
+now you can see this thing in action! Open a new tab and you'll notice that there is a prompt saying that the page was changed by the buildspace os extension. **Make sure to select keep it or it will not work.**<br>
+![](https://i.imgur.com/UtvBBmW.png)
+
+## got questions?
+for any questions please send an email to support@buildspace.so
---
+++ buildspace_TherapAI/test01/app.py
@@ -0,0 +1,58 @@
+from flask import Flask, request, jsonify, render_template
+import openai
+import os
+from dotenv import load_dotenv
+
+app = Flask(__name__)
+
+load_dotenv()
+
+TOKEN = os.getenv('OPENAI_KEY')
+openai.api_key = TOKEN
+
+# File to store conversation history
+CONVERSATION_FILE = "conversation_history.txt"
+
+def load_conversation_history():
+    messages = []
+    if os.path.exists(CONVERSATION_FILE):
+        with open(CONVERSATION_FILE, "r") as file:
+            for line in file:
+                role, content = line.strip().split(":", 1)
+                messages.append({"role": role, "content": content})
+    return messages
+
+def save_conversation_history(messages):
+    with open(CONVERSATION_FILE, "w") as file:
+        for message in messages:
+            file.write(f"{message['role']}:{message['content']}\n")
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    user_input = request.json.get('message')
+    
+    messages = load_conversation_history()
+    if not messages:
+        messages.append({"role": "system", "content": "You are a helpful Therapist."})
+
+    messages.append({"role": "user", "content": user_input})
+    
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+    )
+    
+    assistant_response = response.choices[0].message['content']
+    
+    messages.append({"role": "assistant", "content": assistant_response})
+    
+    save_conversation_history(messages)
+    
+    return jsonify({"response": assistant_response})
+
+if __name__ == '__main__':
+    app.run(debug=True)
